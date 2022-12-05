7fb6fd118ac148bd7c08bb5f3e0d833eaabea802
Guillermo <musictaste.proyecto@gmail.com>
2022-12-05 20:20:45 +0100

    Vincular Base de Datos y Fondo Inicio

--- app/src/main/java/com/example/musictasteproject/Registro.kt
+++ app/src/main/java/com/example/musictasteproject/Registro.kt
@@ -3,16 +3,32 @@ package com.example.musictasteproject
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
 import android.widget.Button
+import android.widget.EditText
+import android.widget.Toast
+import com.android.volley.Request
+import com.android.volley.Response
+import com.android.volley.toolbox.StringRequest
+import com.android.volley.toolbox.Volley
 
 class Registro : AppCompatActivity() {
     private lateinit var btnCont: Button
+    var txtNombre: EditText?=null
+    var txtApellidos: EditText?=null
+    var txtFecha: EditText?=null
+    var txtDireccion: EditText?=null
+    var txtEmail: EditText?=null
+    var txtPass: EditText?=null
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_registro)
-
-
-
+        txtNombre=findViewById(R.id.EditNombre)
+        txtApellidos=findViewById(R.id.EditApellidos)
+        txtFecha=findViewById(R.id.EditFecha)
+        txtDireccion=findViewById(R.id.EditDireccion)
+        txtEmail=findViewById(R.id.EditEmail)
+        txtPass=findViewById(R.id.EditPassword)
 
 
 
@@ -26,5 +42,27 @@ class Registro : AppCompatActivity() {
             }
 
 
+    }
+    fun clickBtnInsertar(view:View){
+        //URL del mysql Insertar Usuario
+        val url=""
+        val queue=Volley.newRequestQueue(this)
+        var resultadoPost = object : StringRequest(Request.Method.POST,url,
+        Response.Listener<String> { response ->
+            Toast.makeText(this,"Usuario insertado existosamente",Toast.LENGTH_LONG).show()
+        },Response.ErrorListener { error ->
+                Toast.makeText(this, "Error $error", Toast.LENGTH_LONG).show()
+            }){
+            override fun getParams(): MutableMap<String, String> {
+                val parametros=HashMap<String,String>()
+                parametros.put("nombre", txtNombre?.text.toString())
+                parametros.put("apellido", txtApellidos?.text.toString())
+                parametros.put("fechaNacimiento", txtFecha?.text.toString())
+                parametros.put("direccion", txtDireccion?.text.toString())
+                parametros.put("email", txtEmail?.text.toString())
+                parametros.put("contrasena", txtPass?.text.toString())
+                return parametros
+            }
+        }
     }
 }
